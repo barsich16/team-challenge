f9e903e07e0dc945a1d4592cb723785ee7f511c2
barsich16 <bohdanborisenko8@gmail.com>
2023-07-13 16:23:01 +0300

    add redux, css reset, routing, prettier, layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import {HomePage} from "./pages/HomePage/HomePage";
+import {Layout} from "./layouts/Layout";
+import {Route, Routes} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      Team challenge
-    </div>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              {/*<Route path="about" element={<About />} />*/}
+              {/*<Route path="dashboard" element={<Dashboard />} />*/}
+
+              {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+              {/*<Route path="*" element={<NoMatch />} />*/}
+          </Route>
+      </Routes>
   );
 }
 
