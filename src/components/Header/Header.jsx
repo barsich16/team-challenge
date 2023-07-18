744abdc1ac80c3f5ce8d4f46d555c4946acecbfa
barsich16 <bohdanborisenko8@gmail.com>
2023-07-18 16:27:14 +0300

    start creating header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,13 @@
+import styles from './Header.module.scss'
+
 export const Header = () => {
-    
-    return <div>Header</div>;
+
+    return <div>
+        Header
+        <div className={styles.wrapper}>
+            <div className={styles.modal}>
+                <div>content</div>
+            </div>
+            <div className={styles.overlay}></div>
+        </div></div>;
 };
