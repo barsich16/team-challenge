ed75bea00ad01b81a61e9160e09317ef960730a0
Sergey Stepanov <101949638+SergeyS007@users.noreply.github.com>
2023-07-19 13:46:05 +0300

    Added tabContainer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import {HomePage} from "./pages/HomePage/HomePage";
-import {Layout} from "./layouts/Layout";
-import {Route, Routes} from "react-router-dom";
+import { HomePage } from './pages/HomePage/HomePage';
+import { Layout } from './layouts/Layout';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-      <Routes>
-          <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              {/*<Route path="about" element={<About />} />*/}
-              {/*<Route path="dashboard" element={<Dashboard />} />*/}
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<HomePage />} />
+				{/*<Route path="about" element={<About />} />*/}
+				{/*<Route path="dashboard" element={<Dashboard />} />*/}
 
-              {/* Using path="*"" means "match anything", so this route
+				{/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-              {/*<Route path="*" element={<NoMatch />} />*/}
-          </Route>
-      </Routes>
-  );
+				{/*<Route path="*" element={<NoMatch />} />*/}
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
---
+++ src/components/TabContent/TabContent.jsx
@@ -0,0 +1,6 @@
+export const TabContent = ({ title, content }) => (
+	<div className='tabcontent'>
+		<h3>{title}</h3>
+		<p>{content}</p>
+	</div>
+);
