9ebc04d57e479e6c98c76c61ed867d83adc9fd3d
barsich16 <bohdanborisenko8@gmail.com>
2023-07-20 16:29:22 +0300

    add photo, color buttons

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,56 +1,37 @@
 import styles from './CardItem.module.scss';
 import src from './iphone.png';
+import cs from 'classnames';
+import {useState} from "react";
 
-export const CardItem = () => {
-	const data = [
-		{
-			colorName: 'green',
-			colorValue: '#333',
-			image: 'png',
-			name: 'Apple iPhone 14 Pro Max 512GB Deep Purple...',
-			price: 69999,
-			oldPrice: 75999,
-			rating: 4,
-			reviews: 244,
-			discount: 8,
-		},
-		{
-			colorName: 'red',
-			colorValue: '#666',
-			image: '/iphone.png',
-			name: 'Apple iPhone 14 Pro Max 512GB Purple...',
-			price: 79999,
-			oldPrice: 85999,
-			rating: 5,
-			reviews: 245,
-			discount: 9,
-		},
-	];
+export const CardItem = ({data}) => {
+    const [activeItem, setActiveItem] = useState(data[0]);
 
-	return (
-		<>
-			{data.map((item, index, array) => (
-				<div className={styles.main}>
-					<div className={styles.image_block}>
-						<img src={src} alt='' />
-					</div>
-					<div className={styles.colors}>
-						{array.map((item) => (
-							<span>{item.colorName}</span>
-						))}
-					</div>
-					<h3 className={styles.name}>{item.name}</h3>
-					<div className={styles.reviews}></div>
-					<p className={styles.old_price}></p>
-					<div className={styles.info}>
+
+    return (
+        <>
+            <div className={styles.main}>
+                <div className={styles.image_block}>
+                    <img src={src} alt=''/>
+                </div>
+                //Todo: при кліку на колір міняти іншу картку
+                <div className={styles.colors}>
+                    {data.map((item) => (
+                        <span onClick={() => setActiveItem(item)}
+                              className={cs(styles.color, {[styles.color_active]: item.id == activeItem.id})}
+                              style={{backgroundColor: item.colorValue}}></span>
+                    ))}
+                </div>
+                <h3 className={styles.name}>{activeItem.name}</h3>
+                <div className={styles.reviews}></div>
+                <p className={styles.old_price}></p>
+                <div className={styles.info}>
 						<span className={styles.price}>
 							<span className={styles.price_old}></span>
 							<span className={styles.price_discount}></span>
 						</span>
-						<span>image</span>
-					</div>
-				</div>
-			))}
-		</>
-	);
+                    <span>image</span>
+                </div>
+            </div>
+        </>
+    );
 };
