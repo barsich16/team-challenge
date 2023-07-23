1fd304dc9776fe980b4face56d9a5fb85161780f
barsich16 <bohdanborisenko8@gmail.com>
2023-07-23 23:15:51 +0300

    add ui/tooltip

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,45 +1,57 @@
 import styles from './CardItem.module.scss';
-import {ReactComponent as Star} from "../../assets/icons/star.svg";
+import { ReactComponent as Star } from '../../assets/icons/star.svg';
 import src from './iphone.png';
 import cs from 'classnames';
-import {useState} from 'react';
+import { useState } from 'react';
+import { ToolTip } from '../UI/Tooltip/Tooltip';
 
-export const CardItem = ({data}) => {
-    const [activeItem, setActiveItem] = useState(data[0]);
+export const CardItem = ({ data }) => {
+	const [activeItem, setActiveItem] = useState(data[0]);
 
-    return (
-        <>
-            <div className={styles.main}>
-                <div className={styles.image_block}>
-                    <img src={src} alt=''/>
-                </div>
-                <div className={styles.colors}>
-                    {data.map((item) => (
-                        <span
-                            onClick={() => setActiveItem(item)}
-                            className={cs(styles.color, {
-                                [styles.color_active]: item.id == activeItem.id,
-                            })}
-                            style={{backgroundColor: item.colorValue}}
-                        ></span>
-                    ))}
-                </div>
-                <h3 className={styles.name}>{activeItem.name}</h3>
-                <div className={styles.reviews}>
-                    {new Array(5).fill().map((_, index) => <Star
-                        className={cs({[styles.star_disabled]: activeItem.rating <= index})}/>
-                    )}
-                    <span className={styles.review}>{activeItem.reviews} review{activeItem.reviews > 1 && "s"}</span>
-                </div>
-                <p className={styles.old_price}></p>
-                <div className={styles.info}>
-					<span className={styles.price}>
-						<span className={styles.price_old}></span>
-						<span className={styles.price_discount}></span>
+	return (
+		<>
+			<div className={styles.main}>
+				<div className={styles.image_block}>
+					<img src={src} alt='' />
+				</div>
+				<div className={styles.colors}>
+					{data.map((item) => (
+						<span
+							onClick={() => setActiveItem(item)}
+							className={cs(styles.color, {
+								[styles.color_active]: item.id == activeItem.id,
+							})}
+							style={{ backgroundColor: item.colorValue }}
+						></span>
+					))}
+				</div>
+				<h3 className={styles.name}>{activeItem.name}</h3>
+				<div className={styles.reviews}>
+					{new Array(5).fill().map((_, index) => (
+						<Star
+							className={cs({
+								[styles.star_disabled]: activeItem.rating <= index,
+							})}
+						/>
+					))}
+					<span className={styles.review}>
+						{activeItem.reviews} review{activeItem.reviews > 1 && 's'}
 					</span>
-                    <span>image</span>
-                </div>
-            </div>
-        </>
-    );
+				</div>
+
+				<div className={styles.buy}>
+					<div className={styles.price}>
+						<div>200990</div>
+						<div className={styles.price_old}>18 999</div>
+					</div>
+					{/*<div className={styles.discount}>*/}
+					<ToolTip text={'Promotional offer'}>
+						<div>8%</div>
+					</ToolTip>
+					{/*</div>*/}
+					<span>buy</span>
+				</div>
+			</div>
+		</>
+	);
 };
