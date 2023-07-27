dc4e58f520c3bde3ab86c8d7a6d39db6ddf55b4d
Sergey Stepanov <101949638+SergeyS007@users.noreply.github.com>
2023-07-28 00:00:37 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,40 @@
 import css from './Footer.module.css';
+import { FiInstagram } from 'react-icons/fi';
+import { LuTwitter } from 'react-icons/lu';
+import { FaFacebookF } from 'react-icons/fa';
+
 export const Footer = () => {
+	return (
+		<div className={css.container}>
+			<div>
+				<div className={css.logo}></div>
+				<div className={css.slogan}>Motto, company slogan.</div>
+				<div className={css.localisation}></div>
+				<div className={css.icons}>
+					<FiInstagram fill='white' size={20} />
+					<LuTwitter fill='white' size={20} />
+					<FaFacebookF fill='white' size={20} />
+				</div>
+				<p className={css.text}>NAME 2022. All Rights Reserved.</p>
+			</div>
+			<div className={css.menu}>
+				<p>menu1</p>
+				<p>menu2</p>
+				<p>menu3</p>
+				<p>menu4</p>
+				<p>menu5</p>
+			</div>
+			<div className={css.menu2}>
+				<p>menu6</p>
+				<p>menu7</p>
+				<p>menu8</p>
+				<p>menu9</p>
+				<p>menu10</p>
+			</div>
+			<div className={css.bottom}>
+				<p className={css.bottom_text}>Cookie Policy</p>
+				<p className={css.bottom_text}>Privacy Policy</p>
+			</div>
+		</div>
 	);
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,39 @@
+import styles from './Header.module.scss';
+import { useState } from 'react';
+
 export const Header = () => {
-    return <div>Header</div>;
+	let [isShow, setIsShow] = useState(false);
+	const showOverlay = () => {
+		setIsShow(true);
+	};
+
+	const hideOverlay = () => {
+		setIsShow(false);
+	};
+
+	return (
+		<>
+			<header
+				className={`${styles.header} ${isShow ? styles.header_open : ''}`}
+			>
+				<HeaderItem isActive showOverlay={showOverlay}>
+					Burger
+				</HeaderItem>
+				<HeaderItem isActive showOverlay={showOverlay}>
+					Searchbar
+				</HeaderItem>
+				<HeaderItem isActive showOverlay={showOverlay}>
+					Cart
+				</HeaderItem>
+			</header>
+			<div
+				onClick={hideOverlay}
+				className={`${styles.wrapper} ${isShow ? styles.wrapper_open : ''}`}
+			/>
+		</>
+	);
+};
+
+const HeaderItem = ({ showOverlay, children }) => {
+	return <div onClick={showOverlay}>{children}</div>;
 };
