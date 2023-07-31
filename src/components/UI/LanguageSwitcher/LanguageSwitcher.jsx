d3ec269200d18dab080ccae17bee5c81909e16d2
barsich16 <bohdanborisenko8@gmail.com>
2023-07-31 16:52:01 +0300

    add language switcher

--- src/components/UI/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/components/UI/LanguageSwitcher/LanguageSwitcher.jsx
@@ -2,28 +2,33 @@ import styles from './LanguageSwitcher.module.scss'
 import cs from 'classnames'
 import {useState} from "react";
 
-export const LanguageSwitcher = () => {
-    const [activeLang, setActiveLang] = useState('en');
+export const LanguageSwitcher = ({locale = 'en', theme = 'light'}) => {
+    const [activeLang, setActiveLang] = useState(locale);
 
-    const handleLanguageChange = (lang) => {
-        setActiveLang(lang);
+    const handleLanguageToggle = () => {
+        setActiveLang((prevLang) => (prevLang === 'en' ? 'ua' : 'en'));
     };
 
     return (
-        <div className={styles.languageSwitcher}>
-            <button
-                className={cs(styles.langButton, styles.en, {[styles.active]: activeLang === 'en'})}
-                // className={`${styles.langButton} ${activeLang === 'en' ? 'active' : ''} en`}
-                onClick={() => handleLanguageChange('en')}
-            >
-                EN
-            </button>
-            <button
-                className={cs(styles.langButton, styles.ua, {[styles.active]: activeLang === 'ua'})}
-                onClick={() => handleLanguageChange('ua')}
-            >
-                UA
-            </button>
+        <div className={cs(styles.toggle, styles[`toggle_${theme}`])}>
+            <div className={styles.btn_wrapper}>
+                <div
+                    className={cs(styles.activeBg, styles[`activeBg_${theme}`], styles[activeLang])}
+                    onClick={handleLanguageToggle}
+                />
+                <button
+                    className={cs(styles.toggle_button, styles[`toggle_button_${theme}`], {[styles.active]: activeLang === 'en'})}
+                    onClick={handleLanguageToggle}
+                >
+                    EN
+                </button>
+                <button
+                    className={cs(styles.toggle_button, styles[`toggle_button_${theme}`], {[styles.active]: activeLang === 'ua'})}
+                    onClick={handleLanguageToggle}
+                >
+                    UA
+                </button>
+            </div>
         </div>
     );
 };
