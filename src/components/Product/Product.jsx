e30b1151aec8b608fae16f37d07a2b63709134e0
barsich16 <bohdanborisenko8@gmail.com>
2023-08-01 16:07:06 +0300

    add title, stars, tabs

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,30 @@
+import cs from 'classnames';
+import styles from './Product.module.scss';
+import {Breadcrumbs} from "../UI/Breadcrumbs/Breadcrumbs";
+import {ReactComponent as Star} from "../../assets/icons/star.svg";
+import {Tabs} from "../TabContainer/TabContainer";
+
+export const Product = () => {
+    const product = {
+        rating: 4,
+    }
+    return <div className={cs('wrapper', styles.main)}>
+        <Breadcrumbs/>
+        <div className={styles.name_rating}>
+            <h1 className={styles.title}>Apple iPhone 14 Pro Max 512GB Deep Purple (MQ9X3)</h1>
+            <div className={styles.stars}>
+                {new Array(5).fill().map((_, index) => (
+                    <Star
+                        className={cs({
+                            [styles.star_disabled]: product.rating <= index,
+                        })}
+                        width={32}
+                        height={32}
+                    />
+                ))}
+                <span className={styles.stars_count}>(312)</span>
+            </div>
+        </div>
+        <Tabs/>
+    </div>;
+};
--- src/components/TabContainer/TabContainer.jsx
+++ src/components/TabContainer/TabContainer.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import { TabContent } from '../TabContent/TabContent';
-import css from './TabContainer.module.css';
+import React, {useState} from 'react';
+import {TabContent} from '../TabContent/TabContent';
+import css from './TabContainer.module.scss';
+import cs from "classnames";
 
-export const TabContainer = ({ items }) => {
-	const [active, setActive] = React.useState(0);
+export const Tabs = () => {
+    const items = [
+        {tabName: 'General', content: <div>General</div>},
+        {tabName: 'Specifications', content: <div>Specifications</div>},
+        {tabName: 'Reviews & Questions (324)', content: <div>Reviews & Questions</div>},
+        {tabName: 'Accessories', content: <div>Accessories</div>},
+        {tabName: 'Availability', content: <div>Availability</div>}
+    ]
+    const [active, setActive] = useState(0);
 
-	const openTab = (e) => {
-		setActive(e.target.dataset.index);
-	};
+    const openTab = (e) => {
+        console.log(e.target);
+        setActive(e.target.dataset.index);
+    };
 
-	return (
-		<div>
-			<div className={css.container}>
-				{items.map((item, i) => (
-					<button
-						className={i === Number(active) ? css.active : css.btn}
-						onClick={openTab}
-						data-index={i}
-						key={i}
-					>
-						{item.tab}
-					</button>
-				))}
-			</div>
-			{items[active] && <TabContent {...items[active]} />}
-		</div>
-	);
+    return (
+        <div>
+            <div className={css.container}>
+                {items.map((item, i) => (
+                    <button
+                        className={cs(css.btn, {[css.active]: i === Number(active)})}
+                        onClick={openTab}
+                        data-index={i}
+                        key={i}
+                    >
+                        {item.tabName}
+                    </button>
+                ))}
+            </div>
+            {items[active] && items[active].content}
+        </div>
+    );
 };
