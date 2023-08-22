9457217c8a18b175555b21dd252343be5cf59f6a
barsich16 <bohdanborisenko8@gmail.com>
2023-08-22 16:29:51 +0300

    add card in specifications

--- src/components/UI/Price/Price.jsx
+++ src/components/UI/Price/Price.jsx
@@ -1,16 +1,25 @@
 import styles from './Price.module.scss';
-import { ToolTip } from '../Tooltip/Tooltip';
+import {ToolTip} from '../Tooltip/Tooltip';
+import cs from "classnames";
 
-export const Price = ({ actual, old, discountPercent }) => {
-	return (
-		<div className={styles.price}>
-			<div className={styles.price_value}>{actual}</div>
-			<div className={styles.price_old}>{old}</div>
-			<div className={styles.price_discount}>
-				<ToolTip text={'Promotional offer'}>
-					<div className={styles.price_discount_value}>-{discountPercent}%</div>
-				</ToolTip>
-			</div>
-		</div>
-	);
+export const Price = ({size = 'small', actual, old, discountPercent}) => {
+    const defaultStyles = cs(
+        styles.price,
+        styles[`${size}`],
+        // styles[`button_${variant}`],
+    );
+
+    return (
+        <div className={defaultStyles}>
+            <div className={styles.price_old}>{old}</div>
+            <div className={styles.price_value}>
+                <span>{actual}</span>
+                <div className={styles.price_discount}>
+                    <ToolTip text={'Promotional offer'}>
+                        <div className={styles.price_discount_value}>-{discountPercent}%</div>
+                    </ToolTip>
+                </div>
+            </div>
+        </div>
+    );
 };
