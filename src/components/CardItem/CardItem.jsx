1030fa868ca42892b5055df7ed1de5c3c761a157
Sergey Stepanov <101949638+SergeyS007@users.noreply.github.com>
2023-08-31 00:06:20 +0300

    footer corrected

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,71 +1,70 @@
 import styles from './CardItem.module.scss';
-import {ReactComponent as Star} from '../../assets/icons/star.svg';
-import {ReactComponent as Cart} from '../../assets/icons/cart.svg';
+import { ReactComponent as Star } from '../../assets/icons/star.svg';
+import { ReactComponent as Cart } from '../../assets/icons/cart.svg';
 
 import src from './iphone.png';
 import cs from 'classnames';
-import {useState} from 'react';
-import {ToolTip} from '../UI/Tooltip/Tooltip';
-import {Button} from "../UI/Button/Button";
+import { useState } from 'react';
+import { ToolTip } from '../UI/Tooltip/Tooltip';
+import { Button } from '../UI/Button/Button';
 
-export const CardItem = ({data}) => {
-    const [activeItem, setActiveItem] = useState(data[0]);
+export const CardItem = ({ data }) => {
+	const [activeItem, setActiveItem] = useState(data[0]);
 
-    return (
-        <>
-            <div className={styles.main}>
-                <div className={styles.content}>
-                    <div className={styles.btn_group}>
-                        <Button type='like'/>
-                        <Button type='compare'/>
-                    </div>
-                    
-                    <div className={styles.image_block}>
-                        <img src={src} alt=''/>
-                    </div>
-                    <div className={styles.colors}>
-                        {data.map((item) => (
-                            <span
-                                onClick={() => setActiveItem(item)}
-                                className={cs(styles.color, {
-                                    [styles.color_active]: item.id == activeItem.id,
-                                })}
-                                style={{backgroundColor: item.colorValue}}
-                            ></span>
-                        ))}
-                    </div>
-                    <h3 className={styles.name}>{activeItem.name}</h3>
-                    <div className={styles.reviews}>
-                        {new Array(5).fill().map((_, index) => (
-                            <Star
-                                className={cs({
-                                    [styles.star_disabled]: activeItem.rating <= index,
-                                })}
-                            />
-                        ))}
-                        <span className={styles.review}>
-						{activeItem.reviews} review{activeItem.reviews > 1 && 's'}
-					</span>
-                    </div>
+	return (
+		<>
+			<div className={styles.main}>
+				<div className={styles.content}>
+					<div className={styles.btn_group}>
+						<Button type='like' />
+						<Button type='compare' />
+					</div>
 
-                    <div className={styles.buy}>
-                        <div className={styles.price}>
-                            <div className={styles.price_value}>69 999</div>
-                            <div className={styles.price_old}>75 999</div>
-                            <div className={styles.price_discount}>
-                                <ToolTip text={'Promotional offer'}>
-                                    <div className={styles.price_discount_value}>-8%</div>
-                                </ToolTip>
-                            </div>
-                        </div>
+					<div className={styles.image_block}>
+						<img src={src} alt='' />
+					</div>
+					<div className={styles.colors}>
+						{data.map((item) => (
+							<span
+								onClick={() => setActiveItem(item)}
+								className={cs(styles.color, {
+									[styles.color_active]: item.id === activeItem.id,
+								})}
+								style={{ backgroundColor: item.colorValue }}
+							></span>
+						))}
+					</div>
+					<h3 className={styles.name}>{activeItem.name}</h3>
+					<div className={styles.reviews}>
+						{new Array(5).fill().map((_, index) => (
+							<Star
+								className={cs({
+									[styles.star_disabled]: activeItem.rating <= index,
+								})}
+							/>
+						))}
+						<span className={styles.review}>
+							{activeItem.reviews} review{activeItem.reviews > 1 && 's'}
+						</span>
+					</div>
 
-                        <button className={styles.cart}>
-                            <Cart/>
-                        </button>
-                    </div>
-                </div>
+					<div className={styles.buy}>
+						<div className={styles.price}>
+							<div className={styles.price_value}>69 999</div>
+							<div className={styles.price_old}>75 999</div>
+							<div className={styles.price_discount}>
+								<ToolTip text={'Promotional offer'}>
+									<div className={styles.price_discount_value}>-8%</div>
+								</ToolTip>
+							</div>
+						</div>
 
-            </div>
-        </>
-    );
+						<button className={styles.cart}>
+							<Cart />
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,9 @@ import css from './Footer.module.scss';
 import { ReactComponent as Facebook } from '../../assets/icons/facebook.svg';
 import { ReactComponent as Instagram } from '../../assets/icons/instagram.svg';
 import { ReactComponent as Twitter } from '../../assets/icons/twitter.svg';
-// import { ReactComponent as Ua } from '../../assets/icons/UA.svg';
-// import { ReactComponent as En } from '../../assets/icons/EN.svg';
+import { ReactComponent as Ua } from '../../assets/icons/UA.svg';
+import { ReactComponent as En } from '../../assets/icons/EN.svg';
+import { ReactComponent as Email } from '../../assets/icons/email.svg';
 
 export const Footer = () => {
 	return (
@@ -13,29 +14,49 @@ export const Footer = () => {
 					<div className={css.footer_left}>
 						<div className={css.logo}></div>
 						<div className={css.slogan}>Simplify Your Tech Journey</div>
-						{/*<div className={css.localisation}>*/}
-						{/*	<En fill='white' size={26} />*/}
-						{/*	<Ua fill='white' size={26} />*/}
-						{/*</div>*/}
+						<div className={css.localisation}>
+							<En fill='white' size={26} />
+							<Ua fill='white' size={26} />
+						</div>
 						<div className={css.icons}>
 							<Instagram fill='white' size={24} />
 							<Twitter fill='white' size={24} />
 							<Facebook fill='white' size={24} />
 						</div>
 					</div>
-					<div className={css.menu}>
-						<p>menu1</p>
-						<p>menu2</p>
-						<p>menu3</p>
-						<p>menu4</p>
-						<p>menu5</p>
+					<div className={css.footer_center}>
+						<div className={css.menu}>
+							<h2 className={css.footer_center_title}>TechEase</h2>
+							<p>About</p>
+							<p>Vacancies</p>
+							<p>News</p>
+							<p>Customer Support</p>
+						</div>
+						<div className={css.menu2}>
+							<h2 className={css.footer_center_title}>Services</h2>
+							<p>Find your order</p>
+							<p>Service centers</p>
+							<p>Status of the product under repair</p>
+						</div>
+						<div className={css.menu2}>
+							<h2 className={css.footer_center_title}>Information</h2>
+							<p>Delivery and payment</p>
+							<p>Warranty and service</p>
+							<p>Exchange and return of goods</p>
+							<p>Credit</p>
+							<p>Questions and answers</p>
+							<p>Website conditions</p>
+						</div>
 					</div>
-					<div className={css.menu2}>
-						<p>menu6</p>
-						<p>menu7</p>
-						<p>menu8</p>
-						<p>menu9</p>
-						<p>menu10</p>
+
+					<div className={css.footer_right}>
+						<h2 className={css.title}>Subscribe to our newsletter!</h2>
+						<p className={css.footer_right_text}>We will not bother you :)</p>
+						<div className={css.footer_right_email}>example@email.com</div>
+						<button type='button' className={css.footer_right_button}>
+							<Email fill='white' size={24} />
+							<span className={css.button_text}>Subscribe</span>
+						</button>
 					</div>
 				</div>
 				<div className={css.rights}>
